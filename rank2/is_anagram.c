208ffe2f92470dc1ff62ee122cb9042fe44701b6
elinakly <klimova.elina85@gmail.com>
2025-02-03 01:05:46 +0100

    added

--- rank2/is_anagram.c
+++ rank2/is_anagram.c
@@ -1,58 +1,49 @@
-#include <stdio.h>
+#include <unistd.h>
 
-char *to_upper(char *str)
+void	ft_str_tolower(char *s)
 {
-    int i;
+	int i = 0;
 
-    i = 0;
-    while (str[i] != '\0')
-    {
-        if (str[i] >= 'a' && str[i] <= 'z')
-            str[i] -= 32;
-        i++;
-    }
-    return (str);
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'A' && s[i] <= 'Z')
+			s[i] += 32;
+		i++;
+	}
 }
 
-int ft_strlen(char *s)
+int	main(int argc, char **argv)
 {
-    int i = 0;
 
-    while(s[i] != '\0')
-    {
-        i++;
-    }
-    return (i);
-}
+	if (argc != 3)
+		return (1);
 
-int main(int argc, char *argv[])
-{
-    int i;
+	int table[256] = {0};
+	int i = 0;
 
-    i = 0;
-    if (argc != 3)
-        return (1);
-    char *s1 = to_upper(argv[1]);
-    char *s2 = to_upper(argv[2]);
-    int len = ft_strlen (s1);
-    int len2 = ft_strlen (s2);
-    if (len != len2)
-        return (1);
-    while (s1[i] != '\0')
-    {
-        int j = 0;
-        while (s2[j] != '\0')
-        {   
-            if (s1[i] == s2[j])
-            {
-                len--;
-                break;
-            }
-            j++;
-        }
-        i++;
-    }
-    if (len == 0)
-        return (0);
-    return (1);
+	char *s1 = argv[1];
+	char *s2 = argv[2];
+
+	ft_str_tolower(s1);
+	ft_str_tolower(s2);
+
+	while (s1[i] != '\0')
+	{
+		table[(unsigned char)s1[i]] += 1;
+		i++;
+	}
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		table[(unsigned char)s2[i]] -= 1;
+		i++;
+	}
+	i = 0;
+	while (i < 256)
+	{
+		if (table[i] != 0)
+			return (0);
+		i++;
+	}
+	return (0);
 }
