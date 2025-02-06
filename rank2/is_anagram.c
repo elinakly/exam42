c023f458e23bd2b399a5bf5c17fd0a19bce74844
Elina Klymova <eklymova@f0r5s18.codam.nl>
2025-02-06 16:52:55 +0100

    added

--- rank2/is_anagram.c
+++ rank2/is_anagram.c
@@ -7,41 +7,44 @@ char *ft_tolower(char *s)
 	while(s[i] != '\0')
 	{
 		if (s[i] >= 'a' && s[i] <- 'z')
-		{
 			s[i] -= 32;
-		}
 		i++;
 	}
 	return (s);
 }
 
-int	main(int argc, char **argv)
+int is_anagram(char *a, char *b)
 {
-	char *s1, *s2;
+	int table[256] = {0};
 	int i = 0;
-	if (argc != 3)
-		return (1);
-	char table[256] = {0}; 
-	s1 = ft_tolower(argv[1]);
-	s2 = ft_tolower(argv[2]);
 
-	while (s1[i] != '\0')
+	while (a[i] != '\0')
 	{
-		table[s1[i]] += 1;
+		table[a[i]] += 1;
 		i++;
 	}
 	i = 0;
-	while (s2[i] != '\0')
+	while (b[i] != '\0')
 	{
-		table[s2[i]] -= 1;
+		table[b[i]] -= 1;
 		i++;
 	}
 	i = 0;
 	while (i < 256)
 	{
 		if (table[i] != 0)
-			return (1);
+			return(1);
 		i++;
 	}
+	return(0);
+}
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+		return (1);
+	char *s1 = ft_tolower(argv[1]);
+	char *s2 = ft_tolower(argv[2]);
+	if (is_anagram(s1, s2))
+		return(1);
 	return (0);
 }
