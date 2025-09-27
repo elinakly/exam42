b359188f752814d5fde4bef08cc8878aedde84cf
elinakly <klimova.elina85@gmail.com>
2025-09-28 00:31:27 +0200

    hm

--- rank4/picoshell.c
+++ rank4/picoshell.c
@@ -1,24 +1,23 @@
 #include <unistd.h>
-#include <stdlib.h>
 #include <sys/wait.h>
 
-int	picoshell(char **cmds[])
+int picoshell(char **cmds[])
 {
-	pid_t	pid;
-	int		pipefd[2];
-	int		prev_fd;
-	int		i;
+	int pipefd[2];
+	int prev_fd = -1;
+	int i = 0;
+	pid_t pid;
 
-	prev_fd = -1;
-	i = 0;
 	while (cmds[i])
 	{
-		if (cmds[i + 1] && pipe(pipefd))
+		int has_next = (cmds[i + 1] != 0);
+		if (has_next && pipe(pipefd) == -1)
 			return (1);
+
 		pid = fork();
 		if (pid == -1)
 		{
-			if (cmds[i + 1])
+			if (has_next)
 			{
 				close(pipefd[0]);
 				close(pipefd[1]);
@@ -33,7 +32,7 @@ int	picoshell(char **cmds[])
 					exit(1);
 				close(prev_fd);
 			}
-			if (cmds[i + 1])
+			if (has_next)
 			{
 				close(pipefd[0]);
 				if (dup2(pipefd[1], STDOUT_FILENO) == -1)
@@ -45,45 +44,14 @@ int	picoshell(char **cmds[])
 		}
 		if (prev_fd != -1)
 			close(prev_fd);
-		if (cmds[i + 1])
+		if (has_next)
 		{
 			close(pipefd[1]);
 			prev_fd = pipefd[0];
 		}
 		i++;
 	}
-	while (wait(NULL) > 0);
+	while (wait(0) > 0)
+		;
 	return (0);
-}
-
-int picoshell(char **cmds[]);
-
-int main(int argc, char **argv)
-{
-	int cmds_size = 1;
-	for (int i = 1; i < argc; i++)
-	{
-		if (!strcmp(argv[i], "|"))
-			cmds_size++;
-	}
-	char ***cmds = calloc(cmds_size + 1, sizeof(char **));
-	if (!cmds)
-	{
-		dprintf(2, "Malloc error: %m\n");
-		return 1;
-	}
-	cmds[0] = argv + 1;
-	int cmds_i = 1;
-	for (int i = 1; i < argc; i++)
-		if (!strcmp(argv[i], "|"))
-		{
-			cmds[cmds_i] = argv + i + 1;
-			argv[i] = NULL;
-			cmds_i++;
-		}
-	int ret = picoshell(cmds);
-	if (ret)
-		perror("picoshell");
-	free(cmds);
-	return ret;
 }
